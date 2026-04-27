067144097285993fa213b91f0b9d86faad415c19
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2026-04-27 21:44:50 +0200

    fix tests

--- src/guess_explainr/app.py
+++ src/guess_explainr/app.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from litestar import Litestar
 from litestar.logging import LoggingConfig
-from litestar.static_files import StaticFilesConfig
+from litestar.static_files import create_static_files_router
 
 from guess_explainr.routes.index import api_router, router
 
@@ -14,11 +14,12 @@ logging_config = LoggingConfig(
 )
 
 app = Litestar(
-    route_handlers=[router, api_router],
-    static_files_config=[
-        StaticFilesConfig(
-            directories=[BASE_PATH / "static"],
+    route_handlers=[
+        router,
+        api_router,
+        create_static_files_router(
             path="/static",
+            directories=[BASE_PATH / "static"],
         ),
     ],
     logging_config=logging_config,
